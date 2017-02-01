1290f43d4b4dfb4ce9bb4fe532171cb9e90bc4a8
Hsu <edittye379@gmail.com>
2017-02-02 00:48:18 +0800

    require body parser

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // modules
 var express = require('express');
 var http = require('http');
+var bodyParser = require('body-parser'); 
+
+// setup
+var app = express();
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
 
 // server
-var app = express();
 var server = http.createServer(app);
 var port = process.env.PORT || 3000;
 server.listen(port);
@@ -12,4 +17,7 @@ console.log('Server running on port: ' + port);
 // endpoint
 app.get('/', function(req, res){
     res.end('hi');
+});
+app.post('/', function(req, res){
+    res.json(req.body);
 });
