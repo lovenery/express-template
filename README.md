e0be3cb2a8e6beb59e3c1b62e8e2882fa91283c8
Hsu <edittye379@gmail.com>
2017-02-02 17:55:05 +0800

    connect to db & change dir & .env file

---
+++ README.md
@@ -0,0 +1,6 @@
+# Express.js API Template
+
+## Install
+`npm install`  
+`cp .env.json.example .env`
+`npm start` (`npm install nodemon -g`)  
--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 // modules
 var express = require('express');
 var http = require('http');
-var bodyParser = require('body-parser'); 
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 // setup
 var app = express();
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: false}));
+var env = require('./.env.json');
+mongoose.Promise = global.Promise;
+mongoose.connect(env.mongodb.url);
 
 // server
 var server = http.createServer(app);
@@ -21,3 +25,7 @@ app.get('/', function(req, res){
 app.post('/', function(req, res){
     res.json(req.body);
 });
+
+var example = express.Router();
+require('./app/routes/example')(example);
+app.use('/example', example);
