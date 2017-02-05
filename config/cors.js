2e7af92a9093e92e19323a5889412cea1913b8b0
Hsu <edittye379@gmail.com>
2017-02-05 23:35:57 +0800

    spell

--- config/cors.js
+++ config/cors.js
@@ -1,6 +1,6 @@
 module.exports = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorzation,token');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,token');
     next();
 }
